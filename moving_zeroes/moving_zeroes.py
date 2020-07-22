293012ba5397e3cb4b7a62cd9f883050725471cd
alec dye <alecdye@gmail.com>
2020-07-22 12:58:12 -0700

    Complete moving_zeroes challenge

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,14 +1,26 @@
-'''
+"""
 Input: a List of integers
 Returns: a List of integers
-'''
+"""
+
+
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    # Mutate original arr (return arr)
+    # Move any zero values to the end of list (remove & append?)
+    # Single pass time complexity O(1)
+
+    for value in arr:
+        # if value is 0 remove from current position and place at list end
+        if value == 0:
+            arr.remove(value)
+            arr.append(value)
+
+    return arr
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
 
