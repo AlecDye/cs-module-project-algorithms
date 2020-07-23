f4d39d66a90e666b4215b78a8aa6f19ab588e64c
alec dye <alecdye@gmail.com>
2020-07-23 08:27:56 -0700

    Complete window challenge

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,15 +1,31 @@
-'''
+"""
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
-'''
+"""
+
+
 def sliding_window_max(nums, k):
     # Your code here
 
-    pass
+    # need to run a loop on 3 items at a time
+    # find the largest value within those 3 values
+    # return array with largests values from each loop
+
+    max_values = []
+
+    window = [0] * k
+
+    for items in range(0, len(nums) - k + 1):
+        # nums list at position items initialized at to items + k
+        window = nums[items : items + k]
+        # max built in will select the desired largest values
+        max_values.append(max(window))
+
+    return max_values
 
 
-if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+if __name__ == "__main__":
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
